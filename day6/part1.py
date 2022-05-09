2f5723f09ed57d1e5c2ab7a632afa869ba5ab5cd
MetalWarlock <90564702+MetalWarlock@users.noreply.github.com>
2022-05-09 17:02:26 +0700

    Add files via upload

---
+++ day6/part1.py
@@ -0,0 +1,44 @@
+matrix = [[0 for x in range(1000)] for y in range(1000)]
+
+with open('input.txt') as f:
+    things = f.readlines()
+
+for thing in things:
+    if 'on' in thing or 'off' in thing:
+        a = thing.split()
+        instruction = a[1]
+        begin = a[2]
+        end = a[4]
+    else:
+        a = thing.split()
+        instruction = a[0]
+        begin = a[1]
+        end = a[3]
+
+    x_begin, y_begin = begin.split(",")
+    x_end, y_end = end.split(",")
+
+    for x in range(int(x_begin), int(x_end) + 1):
+        for y in range(int(y_begin), int(y_end) + 1):
+
+            if instruction == "toggle":
+                if matrix[x][y] == 0:
+                    matrix[x][y] = 1
+                else:
+                    matrix[x][y] = 0
+
+            if instruction == "on":
+                matrix[x][y] = 1
+
+            if instruction == "off":
+                matrix[x][y] = 0
+
+
+count = 0
+for i in range(1000):
+    for j in range(1000):
+        if matrix[i][j] == 1:
+            count += 1
+
+with open('output1.txt', 'w') as f:
+    print(count, file=f)
